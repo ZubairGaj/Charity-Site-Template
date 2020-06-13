4b06d9b2eff79eb6cff7579510386f050a62f8f0
Zubair Gaj <zubzg@icloud.com>
2020-06-13 23:05:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var fs = require("fs");
 var port = process.env.PORT || 3003;
 var express = require("express");
+console.log(port);
 
 var app = express();
 app.use(express.static(__dirname + "/assets")); //use static files in ROOT/public folder
@@ -9,4 +10,4 @@ app.get('/', (req, res)=>{
   res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(8937);
+app.listen(port);
