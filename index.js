19bc7dc8081295076ae6ee5005f835119696f749
Zubair Gaj <zubzg@icloud.com>
2020-06-13 22:59:00 +0100

    Newchanges

---
+++ index.js
@@ -0,0 +1,12 @@
+var fs = require("fs");
+var port = 8937;
+var express = require("express");
+
+var app = express();
+app.use(express.static(__dirname + "/assets")); //use static files in ROOT/public folder
+
+app.get('/', (req, res)=>{
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(8937);
